b1293968275079ac66e0cda800eb9e178fe01e4e
easygode <bimalbenvenuti@gmail.com>
2023-03-13 18:58:14 +0100

    feat: mustache message

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@
 
 //Vue.JS---
 const app = Vue.createApp ({
-    template: '<h1>Déjà Vue</h1><h2>Hello World</h2>'
+    //template: '<h1>Déjà Vue</h1><h2>Hello World</h2>'
+    data() {
+        return {
+            message: 'I send a message to the world with Vue JS'
+        }
+    }
 })
 
 app.mount('#app');
