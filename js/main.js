5326f0efc7a673b190b575abaa070dc3f69c3da1
easygode <bimalbenvenuti@gmail.com>
2023-03-14 10:35:24 +0100

    esercizio corretto

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 'use strict';
 
-//Vue.JS---
-const app = Vue.createApp ({
-    //template: '<h1>Déjà Vue</h1><h2>Hello World</h2>'
-    data() {
-        return {
-            message: 'I send a message to the world with Vue JS'
+// //Vue.JS---
+// const app = Vue.createApp ({
+//     //template: '<h1>Déjà Vue</h1><h2>Hello World</h2>'
+//     data() {
+//         return {
+//             message: 'I send a message to the world with Vue JS',
+//             image: 'img/Vue.js_Logo_2.svg.png'
+//         }
+//     }
+// })
+
+// app.mount('#app');
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data(){
+        return{
+            title: 'Sono il titolo della pagina',
+            img: 'img/Vue.js_Logo_2.svg.png'
         }
     }
 })
 
-app.mount('#app');
+setTimeout(function(){
+app.mount('#app'); //id #app deve essere presente nell'HTML, come un div generico
+},4000);
   
